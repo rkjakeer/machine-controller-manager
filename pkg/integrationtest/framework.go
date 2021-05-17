43d99dd018be5c71e971e1eb2e65d8c1cb37242b
Jakeer Hussain Rahim Khan <Jakeer.Hussain.Rahim.Khan@sap.com>
2021-05-17 16:22:03 +0530

    Add integrationtest directory

---
+++ pkg/integrationtest/framework.go
@@ -0,0 +1,11 @@
+package integrationtest
+
+// type providerOrphansCheck interface {
+// 	Create() (string, string, error)
+// 	Delete(string) error
+// 	GetExisting() (string, error)
+// 	GetVMs(string) (VMs, error)
+// 	GetVolNames([]corev1.PersistentVolumeSpec) ([]string, error)
+// 	GetUserData() string
+// 	SetUserData(string)
+// }
